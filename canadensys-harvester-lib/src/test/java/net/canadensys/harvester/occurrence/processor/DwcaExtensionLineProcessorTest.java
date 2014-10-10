17eac5d4e89cb45ef07cebe3f747d927d8cf14ab
Christian Gendreau <christiangendreau@gmail.com>
2014-10-10 12:05:30 -0400

    update extension processor test

--- canadensys-harvester-lib/src/test/java/net/canadensys/harvester/occurrence/processor/DwcaExtensionLineProcessorTest.java
+++ canadensys-harvester-lib/src/test/java/net/canadensys/harvester/occurrence/processor/DwcaExtensionLineProcessorTest.java
@@ -11,6 +11,7 @@ import net.canadensys.harvester.ItemProcessorIF;
 import net.canadensys.harvester.config.ProcessingConfigTest;
 import net.canadensys.harvester.occurrence.SharedParameterEnum;
 
+import org.gbif.dwc.terms.GbifTerm;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,11 +37,15 @@ public class DwcaExtensionLineProcessorTest {
 	public void testDwcALineProcessor(){
 		Map<SharedParameterEnum,Object> sharedParameters = new HashMap<SharedParameterEnum, Object>();
 		sharedParameters.put(SharedParameterEnum.SOURCE_FILE_ID, "MySourceFileId");
+		sharedParameters.put(SharedParameterEnum.RESOURCE_UUID, "1-1-1-1");
+		sharedParameters.put(SharedParameterEnum.DWCA_EXTENSION_TYPE, GbifTerm.Multimedia.simpleName());
 		
 		OccurrenceExtensionModel rawModel = new OccurrenceExtensionModel();
 		OccurrenceExtensionModel occModel = ProcessorRunner.runItemProcessor(extLineProcessor, rawModel, sharedParameters);
 		
 		assertEquals("MySourceFileId", occModel.getSourcefileid());
+		assertEquals("1-1-1-1", occModel.getResource_uuid());
+		assertEquals(GbifTerm.Multimedia.simpleName(), occModel.getExt_type());
 		assertTrue(occModel.getAuto_id() > 0);
 	}
 }
